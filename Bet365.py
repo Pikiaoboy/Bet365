43d126d80bbd56b652a951ddeca6f49461d83870
Peter Jones <Peter.Jones@genesisenergy.co.nz>
2018-05-22 16:36:40 +1200

    Update

--- Bet365.py
+++ Bet365.py
@@ -13,17 +13,43 @@ base_file = "C:\\Temp\\Results\\Bet365\\"
 #load browser driver
 driver = webdriver.Chrome()
 driver.maximize_window()
+#loads 365 base page
 driver.get(base_url)
+#loads 365 sports page and waits for load to finish
 driver.find_element_by_xpath("//a[@title='Sports Betting']").click()
 time.sleep(10)
-#driver.find_element_by_xpath("//a[starts-with(@class,'hm-DropDownSelections_Button ')][contains(.,'Odds')]").click()
-#driver.find_element_by_xpath("//a[starts-with(@class,'hm-DropDownSelections_Item ')][contains(.,'Decimal']").click()
+
+#loads Soccer page and waits for load to finish
 driver.find_element_by_xpath("//div[@class='wn-Classification '][contains(.,'Soccer')]").click()
 time.sleep(5)
-#elements = driver.find_elements_by_xpath("//div[@class='sm-CouponLink ']")
-elements = driver.find_elements_by_xpath("//div[starts-with(@class,'sm-CouponLink ')]")
-elements[3].click()
 
+#finds all subpage links from middle page for different leagues
+elements = driver.find_elements_by_xpath("//div[starts-with(@class,'sm-CouponLink ')]")
+
+#loads 4th link for testing
+elements[3].click()
+time.sleep(5)
+
+#finds all subpage links from middle page for different matches
+#recursive testing start from here (after reloading UK List)
 market_groups = driver.find_elements_by_xpath("//div[starts-with(@class,'gl-MarketGroup cm-CouponMarketGroup')]")
+#loads second link for testing
 market_groups[1].find_element_by_xpath(".//div[starts-with(@class,'sl-CouponParticipantWithBookCloses')]").click()
-driver.back()
+
+###ISSUE - element not visible for selenium to expand/contract option
+#doesnt include gl-MarketGroup_Open
+driver.find_elements_by_xpath("//div[starts-with(@class, 'gl-MarketGroupButton ')][not (contains(@class, 'gl-MarketGroup_Open'))]")
+mylen = len(driver.find_elements_by_xpath("//div[starts-with(@class, 'gl-MarketGroupButton ')][not (contains(@class, 'gl-MarketGroup_Open'))]"))
+
+for i in range (0,mylen-1):
+    driver.find_elements_by_xpath("//div[starts-with(@class, 'gl-MarketGroupButton ')][not (contains(@class, 'gl-MarketGroup_Open'))]")[mylen-1-i].click()
+    time.sleep(2)
+
+
+
+
+
+
+#loads code into soup var
+soup = BeautifulSoup(driver.page_source, 'lxml')
+marketgps = soup.findAll("div", class_="gl-MarketGroup ")
